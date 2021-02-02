838954e573a82b1dda2d3569da8440e4d092c41f
japcdev <japc.dev@gmail.com>
2021-02-02 20:09:44 +0100

    Interactive menu options

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -6,7 +6,36 @@ const menuOpts = [
         type: 'list',
         name: 'option',
         message: 'What do you wish to do?',
-        choices: ['opt1', 'opt2', 'opt3']
+        choices: [
+            {
+                value: '1',
+                name: '1. Make a task'
+            },
+            {
+                value: '2',
+                name: '2. Lists all tasks'
+            },
+            {
+                value: '3',
+                name: '3. Lists all complete tasks'
+            },
+            {
+                value: '4',
+                name: '4. Lists all unfinished tasks'
+            },
+            {
+                value: '5',
+                name: '5. Complete task(s)'
+            },
+            {
+                value: '6',
+                name: '6. Delete a task'
+            },
+            {
+                value: '0',
+                name: '0. Exit'
+            },
+        ]
     }
 ]
 
@@ -16,11 +45,24 @@ const inquirerMenu = async() => {
     console.log('====== Seleccione una opción ======'.green);
     console.log('===================================\n'.green);
 
-    const opt = await inquirer.prompt(menuOpts);
-    return opt;
+    // const opt = await inquirer.prompt(menuOpts);
+    // console.log(opt);
+    const { option } = await inquirer.prompt(menuOpts);
+    return option;
+}
+
+const pause = async() => {
+    const msg = {
+        type: 'input',
+        name: 'enter',
+        message: `Press ${ 'Enter'.green } for continue`,
+    }
+
+    await inquirer.prompt(msg);
 }
 
 module.exports = {
-    inquirerMenu
+    inquirerMenu,
+    pause
 }
 
